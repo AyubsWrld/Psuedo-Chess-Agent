18ef462ec259fda2d71507716d75306fcf984c80
Eric <ericpersabusiness@gmail.com>
2025-03-07 22:38:25 -0700

    Modify and Comment: minimax parameters changed to reflect what is actually expected, also fully commented

--- tukvnanawopi.py
+++ tukvnanawopi.py
@@ -14,12 +14,24 @@ class Tukvnanawopi:
     def search_best_move(self) -> Move:
         # call minimax algorithm in minimax.py file
         # return the best move
+        pass
 
-    def minimax(state, depth, alpha, beta, maximizing_player):
+    def minimax(state: np.ndarray, depth: int, alpha: float = -np.inf, beta: float = np.inf, maximizing_player: bool) -> int:
         '''
-        Purpose: The minimax function will explore the state space and find the best possible move for
-        Input:
-        Output
+        Purpose: The minimax function will explore the state space and find the best possible move for the maximizing
+        or minimizing player, as given in the function input.
+
+        Input: the current state of the game given as a numpy 2D array, the current depth of the state given as an integ
+        er, maximizing player, with max representing a move for the white player and min representing a move for the bla
+        ck player, and finally an alpha and beta value (initialized to -inf and inf respectively if no values given)
+
+        Output: As specified by the project description, the output consists of a single move that the agent performs.
+        A move is indicated by a pair of squares, where the first indicates which piece is to be moved, and the second 
+        indicates where the piece is to be moved. For example, in the board configuration above, the horizontal right 
+        move on row 5 will be represented as C5-E5
+
+        Known Bugs/Problems: At the moment this is completely pseudocode. need various functions implemented by classmat
+        es before I can proceed.
         '''
         if depth == 0 or game over in position # add terminal state verification, later on possibly implement a time constraint
             return evaluation of position
